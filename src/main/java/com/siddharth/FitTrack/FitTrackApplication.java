09a52f1c98e2c0223ecce0456dfd843ad9ba4deb
Siddharth <siddharth@Jagritis-MacBook-Air.local>
2024-02-10 01:16:04 +0530

    initial commit

---
+++ src/main/java/com/siddharth/FitTrack/FitTrackApplication.java
@@ -0,0 +1,13 @@
+package com.siddharth.FitTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitTrackApplication.class, args);
+	}
+
+}
